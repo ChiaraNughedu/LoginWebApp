cb4f8a72a992110e3c5f398e41097dbd89ee09c4
ChiaraNughedu <chianughedu@gmail.com>
2025-02-13 17:00:10 +0100

    rcizio finito

--- LoginWebApp/Program.cs
+++ LoginWebApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LoginWebApp;
+
+        bool uscita = false;
+
+        while (!uscita)
+        {
+            Console.Clear();
+            Console.WriteLine("OPERAZIONI");
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Logout");
+            Console.WriteLine("3. Verifica ora e data di login");
+            Console.WriteLine("4. Lista degli accessi");
+            Console.WriteLine("5. Esci");
+            Console.Write("Seleziona un'operazione: ");
+            string scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    Utente.Login();
+                    break;
+                case "2":
+                    Utente.Logout();
+                    break;
+                case "3":
+                    Utente.VerificaOraLogin();
+                    break;
+                case "4":
+                    Utente.ListaAccessi();
+                    break;
+                case "5":
+                    uscita = true;
+                    break;
+                default:
+                    Console.WriteLine("Operazione non valida. Premi un tasto per continuare.");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+  
---
+++ LoginWebApp/Utente.cs
@@ -0,0 +1,94 @@
+﻿namespace LoginWebApp
+{
+    public static class Utente
+    {
+        public static string Username { get; private set; } = null;
+        public static DateTime? LoginTime { get; private set; } = null;
+        public static List<DateTime> AccessList { get; private set; } = new List<DateTime>();
+
+        public static void Login()
+        {
+            if (Username != null)
+            {
+                Console.WriteLine("Errore: Sei già loggato come " + Username);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.Write("Inserisci username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Inserisci password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Conferma password: ");
+            string confermaPassword = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                Console.WriteLine("Errore: Il nome utente non può essere vuoto.");
+            }
+            else if (password != confermaPassword)
+            {
+                Console.WriteLine("Errore: Le password non coincidono.");
+            }
+            else
+            {
+                Username = username;
+                LoginTime = DateTime.Now;
+                AccessList.Add(LoginTime.Value);
+                Console.WriteLine("Login effettuato con successo.");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void Logout()
+        {
+            if (Username == null)
+            {
+                Console.WriteLine("Errore: Nessun utente è loggato.");
+            }
+            else
+            {
+                Console.WriteLine($"Logout effettuato per l'utente {Username}.");
+                Username = null;
+                LoginTime = null;
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void VerificaOraLogin()
+        {
+            if (Username == null || LoginTime == null)
+            {
+                Console.WriteLine("Errore: Nessun utente è loggato.");
+            }
+            else
+            {
+                Console.WriteLine($"Utente {Username} loggato alle: {LoginTime}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ListaAccessi()
+        {
+            if (AccessList.Count == 0)
+            {
+                Console.WriteLine("Nessun accesso registrato.");
+            }
+            else
+            {
+                Console.WriteLine("Lista degli accessi:");
+                foreach (DateTime accesso in AccessList)
+                {
+                    Console.WriteLine(accesso);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
